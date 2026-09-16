9babe1ccf92dec83c326e227a0eb86cd1f1d11a7
AngeloA1022 <angelo.armijo@inacapmail.cl>
2026-09-16 14:28:00 -0300

    intento

--- Pagina_Ang/Pagina_Ang/settings.py
+++ Pagina_Ang/Pagina_Ang/settings.py
@@ -15,8 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- Pagina_Ang/Pokemon/models.py
+++ Pagina_Ang/Pokemon/models.py
@@ -9,3 +9,19 @@ pokemon = [
     {'Nombre':'Greninja','Tipo':'Agua','Imagen':'greninja.png'},
     {'Nombre':'Decidueye','Tipo':'Planta','Imagen':'decidueye.png'},
 ]
+
+
+
+class Pokemon(models.Model):
+    Nombre = models.CharField(max_length=100)
+    Tipo = models.CharField(max_length=100)
+    Descripcion = models.TextField(max_length=500)
+    Imagen = models.ImageField(upload_to='pokemon_images/')
+
+    def __str__(self):
+        return self.Nombre
+
+    class Meta:
+        db_table = 'pokemon'
+        verbose_name = 'Pokemon'
+        verbose_name_plural = 'Pokemons'
