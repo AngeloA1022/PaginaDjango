e26c9ea4d1fb67165c35a89bc5c9dc127d114b9d
AngeloA1022 <angelo.armijo@inacapmail.cl>
2026-09-16 16:22:53 -0300

    funciona

--- Pagina_Ang/Pagina_Ang/urls.py
+++ Pagina_Ang/Pagina_Ang/urls.py
@@ -1,25 +1,9 @@
-"""
-URL configuration for Pagina_Ang project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from Pokemon import views as vista
+from Pokemon import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',vista.inicio,name="inicio"),
-    path('pokemon/',vista.pokemon,name='pokemon'),
+    path('', views.inicio, name='inicio'),
+    path('pokemon/', views.pokemon, name='pokemon'),
 ]
--- Pagina_Ang/Pokemon/admin.py
+++ Pagina_Ang/Pokemon/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Pokemon.models import Pokemon
 
 # Register your models here.
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ['Nombre', 'Tipo', 'Descripcion', 'Imagen']
+
+
+admin.site.register(Pokemon, PokemonAdmin)
