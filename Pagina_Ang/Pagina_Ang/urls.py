aefc9f2e0fb5e5ca320b6aca62716f480e5309dc
AngeloA1022 <angelo.armijo@inacapmail.cl>
2026-09-23 15:31:12 -0300

    cambios

--- Pagina_Ang/Pagina_Ang/urls.py
+++ Pagina_Ang/Pagina_Ang/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('pokemon/', views.pokemon, name='pokemon'),
     path('inicioadmin/', views.inicioadmin, name='inicioadmin'),
-    path('agregar_pokemon/', views.agregar_pokemon, name='agregar_pokemon'),
-
+    path('agregar_pokemon/', views.pokemon, name='agregar_pokemon'),
 ]
